b3db5e1ce0aa67656fe43b8af161b4aa6ade4374
SkiesXR <pkrasnick@gmail.com>
2021-03-14 04:30:13 -0700

    Include loading state in useFetch hook

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -2,20 +2,24 @@ import { useState, useEffect } from 'react'
 
 export const useFetch = (url, options) => {
   const [response, setResponse] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true)
         const res = await fetch(url, options)
         const json = await res.json()
         setResponse(json)
       } catch (error) {
         setError(error)
+      } finally {
+        setIsLoading(false)
       }
     };
     fetchData()
   }, [])
 
-  return { response, error }
+  return { isLoading, response, error }
 }

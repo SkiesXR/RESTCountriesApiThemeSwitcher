f9b1d83b2f8c4a8c103d22bfa6ba8d469027d327
SkiesXR <pkrasnick@gmail.com>
2021-03-13 21:18:59 -0800

    Rename component

---
+++ src/Components/HeaderSection.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
+
+const Heading = styled.h2`
+
+`
+
+const HeaderSection = () => {
+  return (
+    <Container>
+      <Heading>Where in the world?</Heading>    
+    </Container>
+  )
+}
+
+export default HeaderSection
+
+/* Header Section TODOs
+
+- "Where in the world?" text
+- Theme switching section
+- Moon Icon
+
+*/

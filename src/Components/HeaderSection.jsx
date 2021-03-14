5ba8225eb8d4103dbb1919ad376c1e29146a8b17
SkiesXR <pkrasnick@gmail.com>
2021-03-13 22:36:57 -0800

    Style theme switching section

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -11,20 +11,28 @@ const Container = styled.div`
   box-shadow: 0px 10px 13px -7px #e8e8e8, 5px 5px 15px 5px rgba(0,0,0,0);
 `
 
+const ThemeSwitcherSection = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Icon = styled(DarkThemeIcon) `
+  width: 25px;
+  height: 25px;
+  margin-right: 10px;
+`
+
 const HeaderSection = () => {
   return (
     <Container>
       <h2>Where in the world?</h2>
-      <DarkThemeIcon />
-      <span>Dark Mode</span> 
+      <ThemeSwitcherSection>
+        <Icon/>
+        <span>Dark Mode</span> 
+      </ThemeSwitcherSection>
     </Container>
   )
 }
 
 export default HeaderSection
-
-/* Header Section TODOs
-
-- Theme switching section
-
-*/

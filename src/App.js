febaf45bbb171e07f1c3fe7032405c82df857dc9
SkiesXR <pkrasnick@gmail.com>
2021-03-13 20:45:38 -0800

    Add comments designing element hierarchy

--- src/App.js
+++ src/App.js
@@ -5,3 +5,29 @@ function App() {
 }
 
 export default App;
+
+// Header
+  // "Where in the World?"
+  // Theme Switcher
+
+/* Index Page */
+// Search
+// Filter
+// Grid of country cards
+  // Card
+    // Flag image
+    // Country name
+    // Country details
+      // Population
+      // Region
+      // Capital
+
+/* Detail Page */
+// Back Button
+// Country Flag
+// Country Details
+  // Header
+  // Details
+  // Border Countries section
+    // "Border Countries"
+    // Map of countries to links/buttons

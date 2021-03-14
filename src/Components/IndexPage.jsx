5dda48db3f6701b604c200b86a7275816a8ffedf
SkiesXR <pkrasnick@gmail.com>
2021-03-14 05:30:16 -0700

    Add countries grid

--- src/Components/IndexPage.jsx
+++ src/Components/IndexPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+import CountryCard from './CountryCard'
 
-const IndexPage = () => {
+const CountriesGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 25px;
+  margin-top: 100px;
+`
+
+const IndexPage = ({ data }) => {
+  console.log(data)
   return (
-    <div>
-      Index Page
-    </div>
+    <CountriesGrid>
+      {data.map(country => <CountryCard data={country} key={country.alpha3Code}/>)}
+    </CountriesGrid>
   )
 }
 

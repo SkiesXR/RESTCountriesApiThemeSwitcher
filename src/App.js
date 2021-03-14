15b164d9159255159682c2aa06311dbc437791b9
SkiesXR <pkrasnick@gmail.com>
2021-03-13 21:58:34 -0800

    Add global styles

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from 'react-router-dom'
 import HeaderSection from './Components/HeaderSection'
 import DetailPage from './Components/DetailPage'
 import IndexPage from './Components/IndexPage'
+import GlobalStyles from './globalStyles'
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <HeaderSection />
       <Switch>
         <Route exact path='/detail'>
---
+++ src/globalStyles.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components'
+ 
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: Nunito Sans Light, Sans-Serif;
+  }
+
+  h2 {
+    font-family: Nunito Sans SemiBold, Sans-Serif;
+  }
+`
+ 
+export default GlobalStyles

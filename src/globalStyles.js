6533e902fb29df3664df9090a6113a9617c27696
SkiesXR <pkrasnick@gmail.com>
2021-03-13 22:48:22 -0800

    Add font scaling to global styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,14 +1,37 @@
 import { createGlobalStyle } from 'styled-components'
  
 const GlobalStyles = createGlobalStyle`
+  html {
+    font-size: 18px;
+  }
+
+  @media (max-width: 900px) {
+    html { font-size: 15px; }
+  }
+
+  @media (max-width: 400px) {
+    html { font-size: 13px; }
+  }
+
   body {
     margin: 0;
     padding: 0;
     font-family: Nunito Sans Light, Sans-Serif;
   }
 
+  h1 {
+    font-size: 3rem;
+    font-family: Nunito Sans ExtraBold, Sans-Serif;
+  }
+
   h2 {
+    font-size: 2rem;
     font-family: Nunito Sans SemiBold, Sans-Serif;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+    font-family: Nunito Sans Light, Sans-Serif;
   }
 `
  

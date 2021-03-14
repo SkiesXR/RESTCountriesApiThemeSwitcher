51bf69f5e951d8847ad62c0414c3e0ccb03bc7eb
SkiesXR <pkrasnick@gmail.com>
2021-03-14 03:05:36 -0700

    Set conditional box shadow styles based on theme

--- src/Components/HeaderSection.jsx
+++ src/Components/HeaderSection.jsx
@@ -8,12 +8,16 @@ const Container = styled.div`
   align-items: center;
   padding: 0 35px;
   height: 75px;
-  box-shadow: 0px 10px 13px -7px #e8e8e8, 5px 5px 15px 5px rgba(0,0,0,0);
+  background: ${({ theme }) => theme.elementBackground};
+  box-shadow: ${({ theme }) => theme.name === 'Light'
+    ? '0px 10px 13px -7px #e8e8e8, 5px 5px 15px 5px rgba(0,0,0,0)'
+    : '0px 10px 13px -12px #232323, 5px 5px 15px 5px rgba(0,0,0,0)'
+  };
 `
 
 const HeaderSection = ({ theme, setTheme }) => {
   return (
-    <Container>
+    <Container theme={theme}>
       <h3>Where in the world?</h3>
       <ThemeSwitcher theme={theme} setTheme={setTheme}/>
     </Container>

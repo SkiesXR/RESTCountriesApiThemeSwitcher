cb48b0e2c63eeb9d2d9ed2524dd1ccc80fcf897f
SkiesXR <pkrasnick@gmail.com>
2021-03-16 19:44:44 -0700

    Add transition for null state msg

--- src/Components/SearchNullState.jsx
+++ src/Components/SearchNullState.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+/* Styles Begin */
+
+const fadeIn = keyframes`
+  from { opacity: 0; transform: translateY(10%); };
+  to { opacity: 1; transform: translateY(0%); };
+`
 
 const Container = styled.div`
   display:  grid;
   place-items: center;
   width: 100%;
   margin-top: 5rem;
+  animation: ${fadeIn} .25s ease-in;
 `
 
 const Header = styled.h1`
@@ -17,6 +25,8 @@ const Message = styled.span`
   color: ${({ theme }) => theme.color};
   font-weight: 400;
 `
+
+/* Styles End */
 
 const SearchNullState = ({ query }) => {
   return (

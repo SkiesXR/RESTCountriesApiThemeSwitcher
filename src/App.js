d5ba5ccdb621f10e77846b8f0013885814e0789a
SkiesXR <pkrasnick@gmail.com>
2021-03-16 22:16:05 -0700

    Add todo

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ OPTIONAL ADD-ONS
 - Create mixins to reduce code
 - Create CSS constants
 - Use arrow keys to navigate elements
+- Improve border country section responsiveness
 
 CLEANUP
 - Organize CSS

0ca57830e1e2ab4b4ed26699b1aa5fa9408b298d
SkiesXR <pkrasnick@gmail.com>
2021-03-18 19:19:59 -0700

    Fix mixin prop

--- src/Components/Mixins.js
+++ src/Components/Mixins.js
@@ -21,7 +21,7 @@ export const FlexColCenter = css`
 export const FlexColStart = css`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
 `
 
 export const FlexStartCenter = css`

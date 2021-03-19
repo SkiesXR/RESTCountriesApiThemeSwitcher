2b8e82c63862da33f1310d25953d090b8cbd9e49
SkiesXR <pkrasnick@gmail.com>
2021-03-18 19:13:40 -0700

    Reorganize files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App'
-import * as themes from './Theme/schema.json'
+import * as themes from './Themes/themesSchema.json'
 import { setToLS } from './Utils/storage'
 import './index.css'
 

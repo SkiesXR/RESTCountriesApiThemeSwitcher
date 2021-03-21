9befad937769fae067943f403750a7e1f105a21f
SkiesXR <pkrasnick@gmail.com>
2021-03-20 22:08:47 -0700

    Update format utilities list

--- src/Utils/format.js
+++ src/Utils/format.js
@@ -1,3 +1,9 @@
+// Insert commas into a large number
 export function numberWithCommas (x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Generates a comma-separated list of values
+export function createCommaSeparatedList (list, key) {
+  return list.map(listItem => listItem[key]).join(', ')
 }
